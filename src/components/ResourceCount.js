cfe4a66fb4eb3139c7e4d110a51bf662dc1439c9
Rowan Lin <xdwlf32@gmail.com>
2019-03-27 10:19:17 -0700

    create file ResourceCount.test.js to test <ResourceCount /> for rendering without errorand displaying resources and effects correctly when passed in effects as props

--- src/components/ResourceCount.js
+++ src/components/ResourceCount.js
@@ -9,9 +9,11 @@ class ResourceCount extends Component {
     let {eventEffect, resources} = this.props
     let foodEffects = addUpEffects(eventEffect.filter(event=> event.subtype=== 'food'))
     let waterEffects = addUpEffects(eventEffect.filter(event=> event.subtype=== 'water'))
-    return(<div><h4>Resources</h4>
-      <div>Food: {(resources)? resources.food:null} {(foodEffects!==0)? foodEffects: null}</div>
-      <div>Water: {(resources)?resources.water:null} {(waterEffects!==0)? ((waterEffects>0) ? "+": "") + waterEffects: null}</div>
+    return(<div data-test="component-resource-count"><h4>Resources</h4>
+      <div data-test="food-resource">Food: {(resources)? resources.food:null} {(foodEffects!==0)?
+        (<span data-test="compiled-food-effect"> {(foodEffects>0) ? '+' : ''}{foodEffects}</span>): null}</div>
+      <div data-test="water-resource">Water: {(resources)?resources.water:null} {(waterEffects!==0)?
+        (<span data-test="compiled-water-effect">{(waterEffects>0) ? '+' : ''}{waterEffects}</span>): null}</div>
     </div>)
   }
 }
---
+++ src/components/ResourceCount.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import ResourceCount from './ResourceCount'
+
+import {findByTestAttr} from '../test/testUtils'
+
+const defaultProps = {
+  eventEffect: [],
+  resources: {
+    food: 5,
+    water: 10
+  }
+}
+
+const setup = (props) => {
+  const newProps = Object.assign({}, defaultProps, props)
+  return shallow(<ResourceCount {...newProps}/>)
+}
+
+test('<ResourceCount /> renders without error', ()=>{
+  const wrapper = setup();
+  const container = findByTestAttr(wrapper, 'component-resource-count')
+  expect(container.length).toBe(1)
+})
+
+describe('when effects are passed into props', ()=>{
+  let wrapper;
+  const eventEffect = [{type: 'resource',
+  subtype: 'food',
+  number: -1},{type: 'resource',
+  subtype: 'water',
+  number: -3}]
+  beforeEach(()=>{
+    wrapper = setup({eventEffect})
+  })
+  test('food resources are rendered', ()=>{
+    const foodResource = findByTestAttr(wrapper, 'food-resource')
+    expect(foodResource.length).toBe(1)
+  })
+  test('water resources are rendered', ()=>{
+    const waterResource = findByTestAttr(wrapper, 'water-resource')
+    expect(waterResource.length).toBe(1)
+  })
+  test('food resources show correct resources', ()=>{
+    const foodResource = findByTestAttr(wrapper, 'food-resource')
+    expect(foodResource.text()).toContain(5)
+  })
+  test('water resources show correct resources', ()=>{
+    const waterResource = findByTestAttr(wrapper, 'water-resource')
+    expect(waterResource.text()).toContain(10)
+  })
+  test('food resources show correct effect', ()=>{
+    const effect = findByTestAttr(wrapper, 'compiled-food-effect')
+    expect(effect.text()).toContain(-1)
+  })
+  test('water resources show correct effect', ()=>{
+    const effect = findByTestAttr(wrapper, 'compiled-water-effect')
+    expect(effect.text()).toContain(-3)
+  })
+})
