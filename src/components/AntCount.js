530b8e9ee3014af81c1095d26f0dd0b062210990
Rowan Lin <xdwlf32@gmail.com>
2019-03-26 17:17:32 -0700

    add AntCount.test.js in components folder to test for errorless rendering of AntCount.js and the correct display of number count of ants as passed into AntCount's props

--- src/components/AntCount.js
+++ src/components/AntCount.js
@@ -12,14 +12,18 @@ class AntCount extends Component{
     let pupaeEffect = addUpEffects(eventEffect.filter(event => event.subtype === 'pupae'))
     let workerEffect = addUpEffects( eventEffect.filter(event => event.subtype === 'worker') )
     return (
-      <div>
-        <div>Eggs: {ants.eggs} {(eggEffect!==0)? eggEffect: ''}</div>
-        <div>Larvae: {ants.larvae} {(larvaeEffect!==0)? larvaeEffect: ''}</div>
-        <div>Pupae: {ants.pupae} {(pupaeEffect!==0)? pupaeEffect: ''}</div>
-        <div>Worker Ants: {ants.worker} {(workerEffect!==0)? workerEffect: ''}</div>
+      <div data-test="component-ant-count">
+        <div>Eggs: <span data-test="ant-eggs">{ants.eggs}</span> {(eggEffect!==0)? eggEffect: ''}</div>
+        <div>Larvae: <span data-test="ant-larvae">{ants.larvae}</span> {(larvaeEffect!==0)? larvaeEffect: ''}</div>
+        <div>Pupae: <span data-test="ant-pupae">{ants.pupae}</span> {(pupaeEffect!==0)? pupaeEffect: ''}</div>
+        <div>Worker Ants: <span data-test="ant-worker">{ants.worker}</span> {(workerEffect!==0)? workerEffect: ''}</div>
       </div>
     )
   }
 }
 
+AntCount.defaultProps={
+  ants: null,
+  eventEffect: []
+}
 export default AntCount
---
+++ src/components/AntCount.test.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import AntCount from './AntCount'
+
+import {findByTestAttr} from '../test/testUtils'
+
+const defaultProps = {
+  ants: {
+    eggs: 0,
+    larvae: 0,
+    pupae: 0,
+    worker: 0
+  },
+  eventEffect: []
+}
+
+const setup = (props={}) => {
+  const newProps = Object.assign({}, defaultProps, props)
+  return shallow(<AntCount {...newProps} />)
+}
+
+test('renders without error', ()=>{
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, 'component-ant-count')
+  expect(component.length).toBe(1)
+})
+
+describe('displays the correct amount of', ()=>{
+  let wrapper;
+  let ants;
+  beforeEach(()=>{
+    ants= {
+      eggs: 5,
+      larvae: 6,
+      pupae: 10,
+      worker: 30
+    }
+    wrapper= setup({ants})
+  })
+  test('ant eggs', ()=>{
+    const eggCount = findByTestAttr(wrapper, 'ant-eggs')
+    expect(Number(eggCount.text())).toBe(ants.eggs)
+  })
+  test('ant larvae', ()=>{
+    const larvaeCount = findByTestAttr(wrapper, 'ant-larvae')
+    expect(Number(larvaeCount.text())).toBe(ants.larvae)
+  })
+  test('ant pupae', ()=>{
+    const pupaeCount = findByTestAttr(wrapper, 'ant-pupae')
+    expect(Number(pupaeCount.text())).toBe(ants.pupae)
+  })
+  test('ant worker', ()=>{
+    const workerCount = findByTestAttr(wrapper, 'ant-worker')
+    expect(Number(workerCount.text())).toBe(ants.worker)
+  })
+})
