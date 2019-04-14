35a0d816a65c7d80eb949b2bdf81f2340a2d4592
Rowan Lin <xdwlf32@gmail.com>
2019-04-14 12:37:53 -0700

    add comments for readability

--- src/App.js
+++ src/App.js
@@ -9,9 +9,15 @@ import {packUpAndGo} from './utils/travel'
 import {updateChoice, setHome, resetState, setStore, updateNotification} from './actionCreators'
 import {connect} from 'react-redux';
 
+/**
+ * App Component
+ * The container for the entire app
+ */
+
 export class App extends Component {
   constructor(props){
     super(props);
+    //Tracks which page the App should render
     this.state={
       page: 'enter',
     }
@@ -20,21 +26,35 @@ export class App extends Component {
     this.resetGame = this.resetGame.bind(this)
   }
 
+  /**
+   * Changes State of App to render next page
+   * @param {String} page - The page the App should render next
+   */
   changePage(page){
     this.setState({page})
   }
 
+  /**
+   * Handles the logic for the player's choices
+   * @param {String} choice - The choice that the player has chosen in the Gameplay Component
+   */
+
   setChoice(choice){
     this.props.updateChoice(choice)
     if(choice === 'change'){
+      // If the player choose to change homes, set the home to null, limit stores, and change page
       this.props.setHome(null)
-      //redirects to searching
       this.props.setStore(packUpAndGo(this.props.store, this.props.ants))
       this.changePage('searching')
     } else{
+      //If player chooses any other choices, then move on to event page
       this.changePage('event')
     }
   }
+
+  /**
+   * Resets the game by reseting state and changing the page to render the Enter Component
+   */
 
   resetGame(){
     this.props.resetState()
@@ -42,8 +62,9 @@ export class App extends Component {
   }
 
   render() {
-    //logic to figure out which page to render
+    //Logic to figure out which page to render
     let page = loadPage(this.state.page, this.changePage, this.setChoice, this.resetGame)
+    //Checks if game is still playing
     let isGamePlaying = (this.state.page !== 'enter' && this.state.page !== 'prequel')
     return (
       <div className="App" data-test="component-app">
