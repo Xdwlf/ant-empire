d76ca0ab74ddd7da9d4270e2fd6d48a74fd25029
Rowan Lin <xdwlf32@gmail.com>
2019-04-16 10:35:18 -0700

    modify status effects

--- src/utils/general/effects.js
+++ src/utils/general/effects.js
@@ -3,5 +3,35 @@ export const statusEffects = {
     type: "ant",
     subtype: "worker",
     number: 0.6
+  }, {
+    type: "ant",
+    subtype: "pupae",
+    number: 0.6
+  }, {
+    type: "ant",
+    subtype: "larvae",
+    number: 0.8
+  }, {
+    type: "ant",
+    subtype: "egg",
+    number: 0.2
+  }],
+  DEHYDRATED: [{
+    type: "ant",
+    subtype: "worker",
+    number: 0.6
+  }, {
+    type: "ant",
+    subtype: "pupae",
+    number: 0.6
+  }, {
+    type: "ant",
+    subtype: "larvae",
+    number: 0.8
+  }, {
+    type: "ant",
+    subtype: "egg",
+    number: 0.2
   }]
+  
 }
