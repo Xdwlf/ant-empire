a51402db643765b4587296277696ba1fba5868ee
Rowan Lin <xdwlf32@gmail.com>
2019-04-20 22:32:10 -0700

    add informative React Tooltips

--- src/components/AntCount.js
+++ src/components/AntCount.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import {addUpEffects} from '../utils/effect/'
+import ReactTooltip from 'react-tooltip'
+
 import {scale} from '../utils/display/styles'
 import {addStyleToText, styleByValue} from '../utils/display/colorize'
 import './styles/AntCount.css'
@@ -23,26 +25,39 @@ class AntCount extends Component{
         <table>
           <tbody>
             <tr>
-              <th>Eggs:</th>
+              <th data-tip data-for="egg-info">Eggs:</th>
               <td className="data"><span data-test="ant-eggs">{ants.eggs}</span></td>
               <td className="effect">{(eggEffect>0)? addStyleToText('+', scale.okay):''}{(eggEffect!==0)? styleByValue(eggEffect): ''}</td>
             </tr>
             <tr>
-              <th>Larvae:</th>
+              <th data-tip data-for="larvae-info">Larvae:</th>
               <td className="data"><span data-test="ant-larvae">{ants.larvae}</span></td>
               <td className="effect">{(larvaeEffect>0)? addStyleToText('+', scale.okay):''}{(larvaeEffect!==0)? styleByValue(larvaeEffect): ''}</td>
             </tr>
             <tr>
-              <th>Pupae:</th>
+              <th data-tip data-for="pupae-info"> Pupae:</th>
               <td className="data"><span data-test="ant-pupae">{ants.pupae}</span></td>
               <td className="effect">{(pupaeEffect>0)? addStyleToText('+', scale.okay):''}{(pupaeEffect!==0)? styleByValue(pupaeEffect): ''}</td>
             </tr>
             <tr>
-              <th>Workers:</th>
+              <th data-tip data-for="worker-info">Workers:</th>
               <td className="data" data-test="ant-worker">{ants.worker}</td>
-              <td className="effect">{(workerEffect>0)? addStyleToText('+', scale.okay):''}{(workerEffect!==0)? styleByValue(workerEffect): ''}</td></tr>
+              <td className="effect">{(workerEffect>0)? addStyleToText('+', scale.okay):''}{(workerEffect!==0)? styleByValue(workerEffect): ''}</td>
+            </tr>
           </tbody>
         </table>
+        <ReactTooltip className="info-tooltip" id="egg-info" type="light" effect="float">
+                            <span>All ants begin as an egg. Ant eggs are soft, oval, and tiny – about the size of a period at the end of a sentence. Not all eggs are destined to become adults – some are eaten by nestmates for extra nourishment.</span>
+            </ReactTooltip>
+            <ReactTooltip className="info-tooltip" id="larvae-info" type="light" effect="float">
+                            <span>An egg hatches into a worm-shaped larva with no eyes or legs. Larvae are eating machines that rely on adults to provide a constant supply of food. As a result, they grow rapidly, molting between sizes.</span>
+            </ReactTooltip>
+            <ReactTooltip className="info-tooltip" id="pupae-info" type="light" effect="float">
+                            <span>When a larvae is large enough, it metamorphoses into a pupa. This is a stage of rest and reorganization. Pupae look more like adults, but their legs and antennae are folded against their bodies. They start out whitish and gradually become darker.</span>
+            </ReactTooltip>
+            <ReactTooltip className="info-tooltip" id="worker-info" type="light" effect="float">
+                            <span>Worker ants are full-grown when born. Their exoskeletons prevent them from getting any larger. They do not reproduce, but perform other jobs, such as taking care of the brood, building and cleaning the nest, and gathering food. Workers are wingless and typically survive for several months.</span>
+        </ReactTooltip>
       </div>
     )
   }
