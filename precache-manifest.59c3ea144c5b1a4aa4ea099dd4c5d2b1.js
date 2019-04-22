9903b082f932882a29db179496550067ff410d88
Rowan Lin <xdwlf32@gmail.com>
2019-04-21 22:08:02 -0700

    Updates

---
+++ precache-manifest.59c3ea144c5b1a4aa4ea099dd4c5d2b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42cb3283c9d74a245f83",
+    "url": "/ant-empire/static/js/runtime~main.8a5f3164.js"
+  },
+  {
+    "revision": "f3ee7def7dcea130c118",
+    "url": "/ant-empire/static/js/main.62c32ac0.chunk.js"
+  },
+  {
+    "revision": "c819e3b25cf5a8ddf0b7",
+    "url": "/ant-empire/static/js/2.5199946f.chunk.js"
+  },
+  {
+    "revision": "f3ee7def7dcea130c118",
+    "url": "/ant-empire/static/css/main.ebdd66aa.chunk.css"
+  },
+  {
+    "revision": "81387427b3e879c8d0b46a6f8e30cf81",
+    "url": "/ant-empire/index.html"
+  }
+];
